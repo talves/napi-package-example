7724f9b422c0a20c54d8724dac56ee2b69a9145f
talves <tony@3alves.com>
2022-01-19 17:51:54 -0800

    tests: add

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,13 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import { plus100, add } from '../index'
 
-test('sync function from native code', (t) => {
+test('plus100 function from native code', (t) => {
   const fixture = 42
   t.is(plus100(fixture), fixture + 100)
+})
+
+test('add function from native code', (t) => {
+  const fixture = 142
+  t.is(add(100, 42), fixture)
 })
--- simple-test.js
+++ simple-test.js
@@ -1,5 +1,6 @@
 const { plus100 } = require('./index')
 
-console.assert(plus100(0) === 100, 'Simple test failed')
+console.assert(plus100(0) === 100, 'Simple test failed (plus100)')
+console.assert(add(52, 48) === 100, 'Simple test failed (add)')
 
 console.info('Simple test passed')
